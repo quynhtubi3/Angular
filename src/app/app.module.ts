10a9a0fa32b57f0e494424fb89a45dcb5e37ca2c
quynhtubi3 <idkr37@gmail.com>
2024-04-16 09:30:32 +0700

    first_commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { AppOverViewComponent } from './over-view/over-view.component';
+
+@NgModule({
+  declarations: [
+    AppOverViewComponent,
+    // Other components declared here
+  ],
+  imports: [
+    // Other modules imported here
+  ],
+  exports: [
+    AppOverViewComponent,
+    // Other components to export here if needed
+  ]
+})
+export class YourModule { }
